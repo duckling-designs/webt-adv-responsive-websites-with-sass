72cd4de2b5333aca0f0a6214afccf24c696d4cf5
Lisa-Marie <0198@htl.rennweg.at>
2023-10-10 08:30:47 +0200

    added more youtubevideo links

--- index.php
+++ index.php
@@ -3,7 +3,12 @@
 namespace DucklingDesigns\ObjectOrientedPhp;
 require 'vendor/autoload.php';
 
-$youtubeVideo = new YouTubeVideo("What If The World Went Vegetarian?", 'https://www.youtube.com/watch?v=ANUoAdXfA60');
+$youtubeVideo1 = new YouTubeVideo("What If The World Went Vegetarian?", 'https://www.youtube.com/watch?v=ANUoAdXfA60');
+$youtubeVideo2 = new YouTubeVideo("How the food you eat affects your brain - Mia Nacamulli", 'https://www.youtube.com/watch?v=xyQY8a-ng6g');
+$youtubeVideo3 = new YouTubeVideo("Kids vocabulary - World food - Learn English for kids - English educational video", 'https://www.youtube.com/watch?v=4uuGYHfnVRE');
+$youtubeVideo4 = new YouTubeVideo("Why is junk food unhealthy? - Ask Coley - Health Tips for Kids | Educational Videos by Mocomi", 'https://www.youtube.com/watch?v=5iJcLl0sHic');
+$youtubeVideo5 = new YouTubeVideo("Healthy Eating: An introduction for children aged 5-11", 'https://www.youtube.com/watch?v=mMHVEFWNLMc');
+$youtubeVideo6 = new YouTubeVideo("Food Groups for Kids | Learn about the five food groups and their benefits", 'https://www.youtube.com/watch?v=pmgkj01uUTw');
 
 $htmlOutput = <<< HTML
 <!DOCTYPE html>
@@ -17,7 +22,12 @@ $htmlOutput = <<< HTML
 
 <h1>FoodTube</h1>
 <div class="flex-container">
-$youtubeVideo
+$youtubeVideo1
+$youtubeVideo2
+$youtubeVideo3
+$youtubeVideo4
+$youtubeVideo5
+$youtubeVideo6
 </div>
 </body>
 </html>
