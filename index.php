4178e37eb129aee6f0c017b1ded72594e799046c
Lisa-Marie <0198@htl.rennweg.at>
2023-10-17 09:12:11 +0200

    added for() to html output videos

--- index.php
+++ index.php
@@ -3,14 +3,17 @@
 namespace DucklingDesigns\ObjectOrientedPhp;
 require 'vendor/autoload.php';
 
-$youtubeVideo1 = new YouTubeVideo("What If The World Went Vegetarian?", 'https://www.youtube.com/watch?v=ANUoAdXfA60');
-$youtubeVideo2 = new YouTubeVideo("How the food you eat affects your brain - Mia Nacamulli", 'https://www.youtube.com/watch?v=xyQY8a-ng6g');
-$youtubeVideo3 = new YouTubeVideo("Kids vocabulary - World food - Learn English for kids - English educational video", 'https://www.youtube.com/watch?v=4uuGYHfnVRE');
-$youtubeVideo4 = new YouTubeVideo("Why is junk food unhealthy? - Ask Coley - Health Tips for Kids | Educational Videos by Mocomi", 'https://www.youtube.com/watch?v=5iJcLl0sHic');
-$youtubeVideo5 = new YouTubeVideo("Healthy Eating: An introduction for children aged 5-11", 'https://www.youtube.com/watch?v=mMHVEFWNLMc');
-$youtubeVideo6 = new YouTubeVideo("Food Groups for Kids | Learn about the five food groups and their benefits", 'https://www.youtube.com/watch?v=pmgkj01uUTw');
-$vimeoVideo = new VimeoVideo("Banana Bread with Chocolate Chip Streusel", 'https://vimeo.com/groups/bestfoodvideos/videos/150608195');
-
+$videos[] = new YouTubeVideo("What If The World Went Vegetarian?", 'https://www.youtube.com/watch?v=ANUoAdXfA60');
+$videos[] = new YouTubeVideo("How the food you eat affects your brain - Mia Nacamulli", 'https://www.youtube.com/watch?v=xyQY8a-ng6g');
+$videos[] = new YouTubeVideo("Kids vocabulary - World food - Learn English for kids - English educational video", 'https://www.youtube.com/watch?v=4uuGYHfnVRE');
+$videos[] = new YouTubeVideo("Why is junk food unhealthy? - Ask Coley - Health Tips for Kids | Educational Videos by Mocomi", 'https://www.youtube.com/watch?v=5iJcLl0sHic');
+$videos[] = new YouTubeVideo("Healthy Eating: An introduction for children aged 5-11", 'https://www.youtube.com/watch?v=mMHVEFWNLMc');
+$videos[] = new YouTubeVideo("Food Groups for Kids | Learn about the five food groups and their benefits", 'https://www.youtube.com/watch?v=pmgkj01uUTw');
+$videos[] = new VimeoVideo("Banana Bread with Chocolate Chip Streusel", 'https://vimeo.com/groups/bestfoodvideos/videos/150608195');
+$videoString = "";
+foreach ($videos as $video){
+    $videoString .= $video;
+}
 $htmlOutput = <<< HTML
 <!DOCTYPE html>
 <html lang="en">
@@ -20,20 +23,9 @@ $htmlOutput = <<< HTML
     <title>FoodTube</title>
 </head>
 <body>
-
 <h1>FoodTube</h1>
 <div class="flex-container">
-$youtubeVideo1
-$youtubeVideo2
-$youtubeVideo3
-$youtubeVideo4
-$youtubeVideo5
-$youtubeVideo6
-$vimeoVideo
-$vimeoVideo
-$vimeoVideo
-$vimeoVideo
-$vimeoVideo
+$videoString
 </div>
 </body>
 </html>
